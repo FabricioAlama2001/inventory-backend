0a4d34962abf618917cb9f288c208c4558806659
fabricioalama2001 <fabricioalama2001@outlook.es>
2024-03-30 17:08:19 -0500

    Descomentado

--- src/modules/core/controllers/transaction-details.controller.ts
+++ src/modules/core/controllers/transaction-details.controller.ts
@@ -1,70 +1,71 @@
-// import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put } from "@nestjs/common";
-// import { ResponseHttpModel } from "@shared/models";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put } from "@nestjs/common";
+import { ResponseHttpModel } from "@shared/models";
+import { TransactionDetailsService } from "../services/transaction-datails.service";
 
 
-// @Controller('transaction-details')
-// export class TransactionDetailsController {
-//         constructor(private readonly transactionDetailsService:TransactionDetailsService){
+@Controller('transaction-details')
+export class TransactionDetailsController {
+        constructor(private readonly transactionDetailsService:TransactionDetailsService){
 
-//         }   
+        }   
 
-//         @Post()
-//         async create(@Body()payload: any): Promise<ResponseHttpModel> {
-//             const data = await this.transactionDetailsService.create(payload);
+        @Post()
+        async create(@Body()payload: any): Promise<ResponseHttpModel> {
+            const data = await this.transactionDetailsService.create(payload);
 
-//             return {
-//                 data,
-//                 message:'Registro creado',
-//                 title:'',
-//             } 
-//         }
+            return {
+                data,
+                message:'Registro creado',
+                title:'',
+            } 
+        }
 
-//         @Put(':id')
-//         async update(@Param('id') id:string,@Body()payload: any): Promise<ResponseHttpModel> {
-//             const data = await this.transactionDetailsService.update(id, payload);
+        @Put(':id')
+        async update(@Param('id') id:string,@Body()payload: any): Promise<ResponseHttpModel> {
+            const data = await this.transactionDetailsService.update(id, payload);
 
-//             return {
-//                 data,
-//                 message:'Detalle de Transacción actualizado',
-//                 title:'',
-//             } 
-//         }
+            return {
+                data,
+                message:'Detalle de Transacción actualizado',
+                title:'',
+            } 
+        }
 
-//         @Delete(':id')
-//         @HttpCode(HttpStatus.CREATED)
-//         async remove(@Param('id', ParseUUIDPipe) id: string) :Promise<ResponseHttpModel> {
-//         const data = await this.transactionDetailsService.remove(id);
+        @Delete(':id')
+        @HttpCode(HttpStatus.CREATED)
+        async remove(@Param('id', ParseUUIDPipe) id: string) :Promise<ResponseHttpModel> {
+        const data = await this.transactionDetailsService.remove(id);
 
-//         return {
-//             data,
-//             message: `Detail Transaction deleted ${id}`,
-//             title: `Deleted`,
-//         };
-//         }
+        return {
+            data,
+            message: `Detail Transaction deleted ${id}`,
+            title: `Deleted`,
+        };
+        }
                       
-//         @Get()
-//         @HttpCode(HttpStatus.OK)
-//         async findAll() :Promise<ResponseHttpModel> {
-//             const response = await this.transactionDetailsService.findAll();
+        @Get()
+        @HttpCode(HttpStatus.OK)
+        async findAll() :Promise<ResponseHttpModel> {
+            const response = await this.transactionDetailsService.findAll();
             
-//             return {
-//                 data: response,
-//                 message: `index`,
-//                 title: ``,
-//             };
-//         }
+            return {
+                data: response,
+                message: `index`,
+                title: ``,
+            };
+        }
     
-//         @Get(':id')
-//         @HttpCode(HttpStatus.OK)
-//         async findOne(@Param('id', ParseUUIDPipe) id: string) :Promise<ResponseHttpModel> {
-//             const data = await this.transactionDetailsService.findOne(id);
+        @Get(':id')
+        @HttpCode(HttpStatus.OK)
+        async findOne(@Param('id', ParseUUIDPipe) id: string) :Promise<ResponseHttpModel> {
+            const data = await this.transactionDetailsService.findOne(id);
             
-//             return {
-//                 data,
-//                 message: `show ${id}`,
-//                 title: `Success`,
-//             };
-//         }
+            return {
+                data,
+                message: `show ${id}`,
+                title: `Success`,
+            };
+        }
 
 
-// }
+}
